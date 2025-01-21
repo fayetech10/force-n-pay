d1079f28b958aad8536c823a4647f9b0f49cec00
cheikh faye <cheikhfaye150193d@gmail.com>
2025-01-21 13:05:09 +0000

    tailwaid configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+        "./src/**/*.{html,ts}" // Inclut les fichiers HTML et TypeScript
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
